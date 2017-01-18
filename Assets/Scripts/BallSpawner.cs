8e070975f84bfabfebfaa358443abe0c4dff684a
Anton Marin <anton.marin@skoltech.ru>
2017-01-18 16:53:04 +0300

    initial commit

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject BallPrefub;
+    public Transform BallSpawn;
+    public float spawnInterval = 10f;
+    public float ballTimeLife = 10f;
+
+    private float nextTimeBallSpawn;
+
+
+	void Start ()
+    {
+        nextTimeBallSpawn = Time.time + spawnInterval;
+
+    }
+	
+
+	void Update ()
+    {
+		if (Time.time > nextTimeBallSpawn)
+        {
+            nextTimeBallSpawn = Time.time + spawnInterval;
+            GameObject ball = (GameObject)Instantiate(BallPrefub, BallSpawn.position, BallSpawn.rotation);
+            ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(Mathf.PI, 0, 0);
+            ball.GetComponent<Rigidbody>().velocity = BallPrefub.transform.forward * 20f;
+            Destroy(ball, ballTimeLife);
+        }
+	}
+}
