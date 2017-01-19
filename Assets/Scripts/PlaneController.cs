90915475e576e2c0cc3516a7eb3ef8f8acd24cf4
Anton Marin <anton.marin@skoltech.ru>
2017-01-19 13:38:16 +0300

    added assets from area370

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+
+    public float movespeed = 5f;
+
+    public GameObject playPlane;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        float x = Input.GetAxis("Mouse X") * movespeed;
+        float y = Input.GetAxis("Mouse Y") * movespeed;
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(x, 0, y));
+    }
+}
