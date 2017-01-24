8c099173c8cf5316b2b5eedb52f65335d95f1989
GolovanovSergey <mr.golovanov.sergey@yandex.ru>
2017-01-24 16:56:34 +0300

    added newton vr scene customised some settings

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -6,27 +6,31 @@ public class BallSpawner : MonoBehaviour {
 
     public GameObject BallPrefub;
     public Transform BallSpawn;
-    public float spawnInterval = 10f;
-    public float ballTimeLife = 10f;
+    public float spawnInterval = 3f;
+    public float ballTimeLife = 15f;
+    public float velocity = 20f; 
 
     private float nextTimeBallSpawn;
+    public Vector3 gravity;
 
 
 	void Start ()
     {
         nextTimeBallSpawn = Time.time + spawnInterval;
+        
 
     }
 	
 
 	void Update ()
     {
-		if (Time.time > nextTimeBallSpawn)
+        Physics.gravity = gravity;
+        if (Time.time > nextTimeBallSpawn)
         {
             nextTimeBallSpawn = Time.time + spawnInterval;
             GameObject ball = (GameObject)Instantiate(BallPrefub, BallSpawn.position, BallSpawn.rotation);
-            ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(10000*Mathf.PI, 10000*Mathf.PI, 10000*Mathf.PI);
-            ball.GetComponent<Rigidbody>().velocity = BallPrefub.transform.forward * 20f;
+            //ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(10000*Mathf.PI, 10000*Mathf.PI, 10000*Mathf.PI);
+            ball.GetComponent<Rigidbody>().velocity = BallPrefub.transform.forward * velocity;
             Destroy(ball, ballTimeLife);
         }
 	}
--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour {
 
-    public float movespeed = 5f;
+   // public float movespeed = 5f;
 
     //public GameObject playPlane;
     public Transform controller;
@@ -21,7 +21,9 @@ public class PlaneController : MonoBehaviour {
        // float y = Input.GetAxis("Mouse Y") ;
 
        // GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(x, 0, y));
-        GetComponent<Rigidbody>().MovePosition(controller.position);
-        GetComponent<Rigidbody>().MoveRotation(controller.rotation);
+        GetComponent<Rigidbody>().MovePosition(controller.position + new Vector3(-0.03f, -0.02f, 0));
+        GetComponent<Rigidbody>().MoveRotation(controller.rotation * Quaternion.Euler(180, 0, 0));
+        //GetComponent<Rigidbody>().MoveRotation( Quaternion.Euler( 180, 0, 0));
+
     }
 }
